e84be3414b3b6e7ade0266493114e93846316e87
Codingboddy <anthonyprecious36@gmail.com>
2023-08-16 20:47:20 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - write a function that print_alphabeth in lower case
+ */
+void print_alphabeth(void)
+{
+	char letter;
+	for (letters = 'a'; letter <= 'z'; letter++)
+		_putchar letters;
+
+	_putchar ('\n');
+}
