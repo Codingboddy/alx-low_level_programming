dd173751778c821c7ac9b1be026f4c983b7da6a3
Codingboddy <anthonyprecious36@gmail.com>
2023-08-20 07:08:13 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -11,9 +11,10 @@ int main(void)
 	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
 	int i;
 
-	for (i = 0; i < 26, i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar (alp[i]);
 	}
 	putchar ('\n');
 	return (0);
+}
