1c8cbe3d22b554036a8d8e7c8a9bedd53ab5a148
Codingboddy <anthonyprecious36@gmail.com>
2023-08-20 07:04:56 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the lower case alphabeth in reverse
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int i;
+
+	for (i = 0; i < 26, i++)
+	{
+		putchar (alp[i]);
+	}
+	putchar ('\n');
+	return (0);
