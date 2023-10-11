3ec60be484a7703ef137632f60d3fe9cf79c2381
Codingboddy <anthonyprecious36@gmail.com>
2023-10-10 19:51:31 -0700

    postitive_or_negative

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,8 @@
 
 
 void positive_or_negative(int i);
-int largest_numbers (int a, int b, int c);
-void print_remaining_day (int year, int month, int day);
-int convert_day (int month, int day);
+int largest_numbers(int a, int b, int c);
+void print_remaining_day(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
